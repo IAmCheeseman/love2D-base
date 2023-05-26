c30840861587103723158a637f3131c318c187d7
Xander Swan <xanderswan32@gmail.com>
2023-05-26 16:41:05 -0400

    Renamed and added event functions

--- binding.lua
+++ binding.lua
@@ -1,26 +1,66 @@
+
+function love.directorydropped(path)
+    Objects.call_on_all("on_directory_dropped", { path })
+end
+
+function love.displayrotated(index, orientation)
+    Objects.call_on_all("on_display_rotated", { index, orientation })
+end
+
+function love.filedropped(file)
+    Objects.call_on_all("on_file_dropped", { file })
+end
+
+function love.focus(is_focused)
+    Objects.call_on_all("on_window_focus", { is_focused })
+end
+
+function love.mousefocus(is_focused)
+    Objects.call_on_all("on_window_mouse_focus", { is_focused })
+end
+
+function love.resize(w, h)
+    Objects.call_on_all("on_window_resize", { w, h })
+end
+
+function love.visible(is_visible)
+    Objects.call_on_all("on_window_visible", { is_visible })
+end
+
+
 function love.keypressed(key, scancode, is_repeat)
-    Objects.call_on_all("on_key_pressed", { key, scancode, is_repeat })
+    Objects.call_on_all("on_key_press", { key, scancode, is_repeat })
 end
 
 function love.keyreleased(key, scancode)
-    Objects.call_on_all("on_key_released", { key, scancode })
+    Objects.call_on_all("on_key_release", { key, scancode })
 end
 
+function love.textedited(text, start, length)
+    Objects.call_on_all("on_text_edit", { text, start, length })
+end
+
+function love.textinput(text)
+    Objects.call_on_all("on_text_input", { text })
+end
+
+
 function love.mousemoved(x, y, dx, dy, is_touch)
-    Objects.call_on_all("on_mouse_moved", { x, y, dx, dy, is_touch })
+    Objects.call_on_all("on_mouse_move", { x, y, dx, dy, is_touch })
 end
 
 function love.mousepressed(x, y, button, is_touch, presses)
-    Objects.call_on_all("on_mouse_pressed", { x, y, button, is_touch, presses })
+    Objects.call_on_all("on_mouse_press", { x, y, button, is_touch, presses })
 end
 
 function love.mousereleased(x, y, button, is_touch, presses)
-    Objects.call_on_all("on_mouse_released", { x, y, button, is_touch, presses })
+    Objects.call_on_all("on_mouse_release", { x, y, button, is_touch, presses })
 end
 
 function love.wheelmoved(x, y)
-    Objects.call_on_all("on_wheel_moved", { x, y })
+    Objects.call_on_all("on_mouse_wheel_move", { x, y })
 end
+
 
 function love.update(dt)
     Objects.process_objects(dt)
