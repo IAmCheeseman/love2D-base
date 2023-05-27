150ef9039b3bfb326161a6d77d446a6b5c7eb0ac
Xander Swan <xanderswan32@gmail.com>
2023-05-27 08:44:39 -0400

    Better vector file

--- tools/vector.lua
+++ tools/vector.lua
@@ -1,36 +1,38 @@
-local function length(x, y)
+local module = {}
+
+function module.length(x, y)
     return math.sqrt(x^2 + y^2)
 end
 
-local function normalized(x, y)
-    local l = length(x, y)
+function module.normalized(x, y)
+    local l = module.length(x, y)
     if l == 0 then
         return 0, 0
     end
     return x / l, y / l
 end
 
-local function dot(x, y, xx, yy)
+function module.dot(x, y, xx, yy)
     return (x^2 + y^2) + (xx^2 + yy^2)
 end
 
-local function direction_to(x, y, xx, yy)
-    return normalized(x - xx, y - yy)
+function module.direction_to(x, y, xx, yy)
+    return module.normalized(x - xx, y - yy)
 end
 
-local function distance_to(x, y, xx, yy)
-    return length(x - xx, y - yy)
+function module.distance_to(x, y, xx, yy)
+    return module.length(x - xx, y - yy)
 end
 
-local function angle(x, y)
+function module.angle(x, y)
     return math.atan2(y, x)
 end
 
-local function angle_to(x, y, xx, yy)
-    return angle(x - xx, y - yy)
+function module.angle_to(x, y, xx, yy)
+    return module.angle(x - xx, y - yy)
 end
 
-local function get_input_direction(up, left, down, right)
+function module.get_input_direction(up, left, down, right)
     local ix, iy = 0, 0
 
     if love.keyboard.isDown(up) then iy = iy - 1 end
@@ -41,9 +43,9 @@ local function get_input_direction(up, left, down, right)
     return ix, iy
 end
 
-local function rotated(x, y, r)
-    local new_rot = angle(x, y) + r
-    local l = length(x, y)
+function module.rotated(x, y, r)
+    local new_rot = module.angle(x, y) + r
+    local l = module.length(x, y)
 
     local nx = math.cos(new_rot) * l
     local ny = math.sin(new_rot) * l
@@ -51,14 +53,4 @@ local function rotated(x, y, r)
     return nx, ny
 end
 
-return {
-    length = length,
-    normalized = normalized,
-    dot = dot,
-    direction_to = direction_to,
-    distance_to = distance_to,
-    angle = angle,
-    angle_to = angle_to,
-    rotated = rotated,
-    get_input_direction = get_input_direction,
-}
+return module
