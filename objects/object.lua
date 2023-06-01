53f9dca1e1a2ff0290103c7fa318fb00db698909
Xander Swan <xanderswan32@gmail.com>
2023-06-01 16:42:40 -0400

    Update paths

--- objects/object.lua
+++ objects/object.lua
@@ -1,5 +1,5 @@
-local path = (...):gsub("%.objects.object", "")
-local timers = require(path .. ".objects.timer")
+local path = (...):gsub("%objects.object", "")
+local timers = require(path .. "objects.timer")
 
 local module = {
     are_paused = false,
