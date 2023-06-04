392895990f03619be9edbb46b2c574239d5b8df0
iamcheeseman <xanderswan32@gmail.com>
2023-06-04 13:31:12 -0400

    Fixed objects created in create events not being created

--- objects/object.lua
+++ objects/object.lua
@@ -22,7 +22,7 @@ local function deep_copy(t)
 end
 
 local function is_object_paused(object)
-    return module.are_paused and object.pause_mode ~= "never"
+    return (module.are_paused and object.pause_mode ~= "never") or object.pause_mode == "always"
 end
 
 function module.process_object(object, dt)
@@ -37,15 +37,17 @@ function module.process_object(object, dt)
 end
 
 function module.process_objects(dt)
+    -- Add each object first so objects can reference other objects made on the same frame
     for _, object in ipairs(create_queue) do
         table.insert(objects, object)
     end
-    for _, object in ipairs(create_queue) do
+    for i = #create_queue, 1, -1 do
+        local object = create_queue[i]
         if object.on_create then
             object:on_create()
         end
+        table.remove(create_queue, i)
     end
-    create_queue = {}
 
     for _, object in ipairs(objects) do
         if not is_object_paused(object) then
