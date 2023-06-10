217578a4f98911a96c7e96bfa917d4df9368e1db
iamcheeseman <xanderswan32@gmail.com>
2023-06-10 19:24:07 -0400

    Pathfinding

--- objects/room.lua
+++ objects/room.lua
@@ -2,6 +2,12 @@ local module = {}
 
 local path = (...):gsub("objects.room$", "")
 local ldtk = require(path .. "tools.thirdparty.ldtklove.ldtk")
+local grid = require(path .. "tools.thirdparty.jumper.grid")
+local jumper = require(path .. "tools.thirdparty.jumper.pathfinder")
+
+local map = {}
+local pathfinding_grid = {}
+local pathfinder = nil
 
 local current = ""
 local ldtk_path = ""
@@ -51,11 +57,54 @@ function module.get_cell(layer_name, x, y)
         error("Can only test IntGrids.")
     end
 
-    local height = module.level.width / layer.gridSize
+    local width = module.level.width / layer.gridSize
     local layer_x = math.floor(x / layer.gridSize) + 1
     local layer_y = math.floor(y / layer.gridSize)
-    local index = layer_y * height + layer_x
+    local index = layer_y * width + layer_x
     return layer.grid[index]
+end
+
+function module.get_cell_l(layer_name, x, y)
+    local layer = layers[layer_name]
+    if layer.type ~= "IntGrid" then
+        error("Can only test IntGrids.")
+    end
+
+    local width = module.level.width / layer.gridSize
+    local layer_x = x
+    local layer_y = y
+    local index = layer_y * width + layer_x
+    return layer.grid[index]
+end
+
+function module.get_path(layer_name, sx, sy, ex, ey)
+    local layer = layers[layer_name]
+    if layer.type ~= "IntGrid" then
+        error("Can only use IntGrids.")
+    end
+
+    local rsx, rsy = math.floor(sx / layer.gridSize), math.floor(sy / layer.gridSize)
+    local rex, rey = math.floor(ex / layer.gridSize), math.floor(ey / layer.gridSize)
+
+    local path, length = pathfinder:getPath(rsx, rsy, rex, rey)
+    if path == nil then
+        return Vector.direction_between(sx, sy, ex, ey)
+    end
+
+    local n1, n2
+    for node, count in path:iter() do
+        if count == 1 then
+            n1 = node
+        elseif count == 2 then
+            n2 = node
+        else
+            break
+        end
+    end
+
+    return Vector.direction_between(
+        n1:getX() + layer.gridSize / 2, n1:getY() + layer.gridSize / 2, 
+        n2:getX() + layer.gridSize / 2, n2:getY() + layer.gridSize / 2)
 end
 
 local function does_entity_have_tag(entity, tag)
@@ -82,9 +131,9 @@ local function convert_property(property, entity)
     elseif property.type == "Color" then
        local rgb = ldtk.hex2rgb(property.value)
        return {
-        r = rgb[1],
-        g = rgb[2],
-        b = rgb[3],
+            r = rgb[1],
+            g = rgb[2],
+            b = rgb[3],
        }
     end
 
@@ -122,6 +171,46 @@ end
 function ldtk.onLayer(layer)
     layers[layer.id] = layer
     table.insert(layer_draw_order, layer.id)
+end
+
+local function printtable(table)
+    io.write("{")
+    for i, v in ipairs(table) do
+        if type(v) == "table" then
+            printtable(v)
+        else
+            io.write(v .. ",")
+        end
+    end
+    io.write("},\n")
+end
+
+function ldtk.onLevelCreated(level)
+    map = {}
+
+    for y = 1, level.height / 16 do
+        table.insert(map, {})
+        for x = 1, level.width / 16 do
+            local cell = module.get_cell_l("Solids", x, y)
+            local cell_is_walkable = false
+            for _, v in ipairs(level.props.valid_cells) do
+                if cell == v then
+                    cell_is_walkable = true
+                    break
+                end
+            end
+
+            if cell_is_walkable then
+                table.insert(map[y], 0)
+            else
+                table.insert(map[y], 1)
+            end
+        end
+    end
+
+    pathfinding_grid = grid(map)
+    pathfinder = jumper(pathfinding_grid, "THETASTAR", 0)
+    -- pathfinder:setMode('ORTHOGONAL')
 end
 
 return module
